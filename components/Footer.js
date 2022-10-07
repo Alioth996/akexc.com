f0f88c001d493b371b5e1c8705180445c7091735
alex <2844520415@qq.com>
2022-10-07 23:47:49 +0800

    chore: extend html display

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,27 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       <p className={styles.cc}>
-        <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/' rel='noreferrer' target='_blank'>
-          CC BY-NC-SA 4.0
-        </a>{' '}
-        ©<a href='https://github.com/akex4396'>凌天(akex4396)</a>
+        Power by ©
+        <a href='https://www.nextjs.cn/'>
+          {' '}
+          <span className={styles.textLink}>Next.js</span>{' '}
+        </a>
       </p>
-      <a href='https://beian.miit.gov.cn/' rel='noreferrer' target='_blank'>
+      <p className={styles.cc}>
+        <a
+          href='https://creativecommons.org/licenses/by-nc-sa/4.0/'
+          rel='noreferrer'
+          target='_blank'
+          className={styles.textLink}
+        >
+          CC BY-NC-SA 4.0
+        </a>
+        &nbsp; © &nbsp;
+        <a href='https://github.com/akex4396' className={styles.textLink}>
+          凌天(akex4396)
+        </a>
+      </p>
+      <a href='https://beian.miit.gov.cn/' rel='noreferrer' target='_blank' className={styles.textLink}>
         黔ICP备2021003687号-2
       </a>
     </div>
