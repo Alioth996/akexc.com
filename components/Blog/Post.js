571aa348ed22808c3b1e408b4d41d48af79338f0
alex <2844520415@qq.com>
2022-10-16 22:50:22 +0800

    chore: 修改标题元素

--- components/Blog/Post.js
+++ components/Blog/Post.js
@@ -1,15 +1,15 @@
-import styles from '../../styles/Post.module.css';
-import Link from 'next/link';
+import styles from '../../styles/Post.module.css'
+import Link from 'next/link'
 
 const Post = ({ title, date, id }) => {
   return (
     <Link href={`/blog/${id}`}>
       <div className={styles.post}>
-        <p className={styles.title}>{title}</p>
+        <h3 className={styles.title}>{title}</h3>
         <p className={styles.date}>{date}</p>
       </div>
     </Link>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
